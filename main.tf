3b620fc957a5c452f597429bf54e3ca41a13fbb5
devoperations6 <devoperations6@example.com>
2024-06-15 16:04:50 +0530

    added subnet module

--- main.tf
+++ main.tf
@@ -15,3 +15,11 @@ provider "aws" {
 resource "aws_vpc" "ohiovpc" {
  cidr_block = "10.0.0.0/16" 
 }
+#subnet creation
+resource "aws_subnet" "ohiosubnet" {
+  vpc_id = aws_vpc.ohiovpc.id
+  cidr_block = "10.0.0.1.0/24"
+  tags = {
+    Name = ohiosubnet
+  }
+}
